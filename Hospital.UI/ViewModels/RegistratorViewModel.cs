f619a2d54a8eef8a612d603b788e5bee0dd28c7d
AdvanGit <advan8912@gmail.com>
2020-11-25 12:33:09 +0500

    DataGridOperations

--- Hospital.UI/ViewModels/RegistratorViewModel.cs
+++ Hospital.UI/ViewModels/RegistratorViewModel.cs
@@ -12,29 +12,17 @@ using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Data;
 
 namespace Hospital.UI.ViewModels
 {
-    public static class Extensions
-    {
-        public static ObservableCollection<T> ToObservableCollection<T>(this IEnumerable<T> col)
-        {
-            return new ObservableCollection<T>(col);
-        }
-    }
 
     public class RegistratorViewModel : INotifyPropertyChanged
     {
         public RegistratorViewModel()
         {
-            RegTables = new List<UserControl>
-            {
-                new Controls.RegDoctorTable(),
-                new Controls.RegPatientTable()
-            };
             CurrentRegTable = RegTables.ElementAt(0);
             CurrentRegPanel = new RegEntryPanel();
-            dataServicesPatient = new GenericDataServices<Patient>(new HospitalDbContextFactory());
         }
 
         private UserControl _currentRegTable;
@@ -42,60 +30,70 @@ namespace Hospital.UI.ViewModels
         private Patient _selectedPatient;
         private Patient _editingPatient;
         private Staff _selectedStaff;
-        private RelayCommand _insertData;
-        private RelayCommand _editUser;
-        private RelayCommand _editCancel;
-        private RelayCommand _createPatient;
-        private RelayCommand _savePatient;
-        private GenericDataServices<Patient> dataServicesPatient;
-
-        public IEnumerable<Belay> Belays { get; private set; }
 
         public UserControl CurrentRegTable { get => _currentRegTable; set { _currentRegTable = value; OnPropertyChanged(nameof(CurrentRegTable)); } }
         public UserControl CurrentRegPanel { get => _currentRegPanel; set { _currentRegPanel = value; OnPropertyChanged(nameof(CurrentRegPanel)); } }
         public Patient SelectedPatient { get => _selectedPatient; set { _selectedPatient = value; OnPropertyChanged(nameof(SelectedPatient)); } }
         public Patient EditingPatient { get => _editingPatient; set { _editingPatient = value; OnPropertyChanged(nameof(EditingPatient)); } }
-
         public Staff SelectedStaff { get => _selectedStaff; set { _selectedStaff = value; OnPropertyChanged(nameof(SelectedStaff)); } }
+
+        public ObservableCollection<Staff> Doctors { get; } = new ObservableCollection<Staff>();
+        public ObservableCollection<Patient> Patients { get; } = new ObservableCollection<Patient>();
+        public ObservableCollection<Belay> Belays { get; } = new ObservableCollection<Belay>();
+        public List<UserControl> RegTables { get; } = new List<UserControl> { new Controls.RegDoctorTable(), new Controls.RegPatientTable() };
+
+        private GenericDataServices<Patient> dataServicesPatient = new GenericDataServices<Patient>(new HospitalDbContextFactory());
+
+        private RelayCommand _insertData;
+        private RelayCommand _selectRow;
+        private RelayCommand _editUser;
+        private RelayCommand _editCancel;
+        private RelayCommand _createPatient;
+        private RelayCommand _savePatient;
 
         public RelayCommand InsertData { get => _insertData ??= new RelayCommand(async obj => await GetData()); }
         public RelayCommand EditUser
         {
-            get => _editUser ??= new RelayCommand(obj =>
-                {
-                    EditingPatient = (Patient)_selectedPatient.Clone();
-                    CurrentRegPanel = new RegEditPanel();
-                });
+            get => _editUser ??= new RelayCommand(async obj => {
+                EditingPatient = (Patient)SelectedPatient.Clone();
+                CurrentRegPanel = new RegEditPanel();
+                await GetBelays();
+            });
         }
-        public RelayCommand EditCancel { get => _editCancel ??= new RelayCommand(obj => { CurrentRegPanel = new RegEntryPanel(); }); }
+        public RelayCommand EditCancel { get => _editCancel ??= new RelayCommand(obj => CurrentRegPanel = new RegEntryPanel()); }
         public RelayCommand CreatePatient
         {
             get => _createPatient ??= new RelayCommand(async obj => { EditingPatient = new Patient(); CurrentRegPanel = new RegEditPanel(); await GetBelays(); });
         }
         public RelayCommand SavePatient { get => _savePatient ??= new RelayCommand(async obj => await dataServicesPatient.Update(EditingPatient.Id, EditingPatient)); }
-
-        public ObservableCollection<Staff> Doctors { get; set; }
-        public List<UserControl> RegTables { get; set; }
-
-        public ObservableCollection<Patient> Patients { get; set; }
-
-        public async Task GetData()
+        public RelayCommand SelectRow
         {
-            await using (HospitalDbContext db = new HospitalDbContextFactory().CreateDbContext())
+            get => _selectRow ??= new RelayCommand(obj => {
+                if (obj.GetType() == typeof(Staff)) SelectedStaff = (Staff)obj;
+                else if (obj.GetType() == typeof(Patient)) SelectedPatient = (Patient)obj;
+            });
+        }
+
+        private async Task GetData()
+        {
+            using (HospitalDbContext db = new HospitalDbContextFactory().CreateDbContext())
             {
-                var _patients = await db.Patients.Include(p => p.Belay).ToListAsync();
-                Belays ??= await db.Belays.ToListAsync();
-                Patients = _patients.ToObservableCollection();
+                Doctors.Clear();
                 var _doctors = await db.Staffs.Include(s => s.Department).ThenInclude(d => d.Title).ToListAsync();
-                Doctors = _doctors.ToObservableCollection();
+                foreach (Staff staff in _doctors) Doctors.Add(staff);
+
+                Patients.Clear();
+                var _patients = await db.Patients.Include(p => p.Belay).ToListAsync();
+                foreach (Patient patient in _patients) Patients.Add(patient);
             }
         }
-        public async Task GetBelays()
+        private async Task GetBelays()
         {
-            if (Belays == null)
-                await using (HospitalDbContext db = new HospitalDbContextFactory().CreateDbContext())
+            if (Belays.Count == 0)
+                using (HospitalDbContext db = new HospitalDbContextFactory().CreateDbContext())
                 {
-                    Belays = await db.Belays.ToListAsync();
+                    var _belays = await db.Belays.ToListAsync();
+                    foreach (Belay belay in _belays) Belays.Add(belay);
                 }
         }
 
