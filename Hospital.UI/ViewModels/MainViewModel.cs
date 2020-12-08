938e1e069872105a8433a7e331aa3b303d7d9be0
AdvanGit <advan8912@gmail.com>
2020-12-08 21:17:00 +0500

    Refactoring MVVM Architecture

--- Hospital.UI/ViewModels/MainViewModel.cs
+++ Hospital.UI/ViewModels/MainViewModel.cs
@@ -1,69 +1,19 @@
 ﻿using Hospital.EntityFramework;
 using Hospital.UI.Services;
-using System.Collections.ObjectModel;
-using System.ComponentModel;
-using System.Linq;
-using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
-using System.Windows.Controls;
 
 public enum UserAccess { admin, doctor, registrator, manager }
 
 namespace Hospital.UI.ViewModels
 {
-    public class MainViewModel : INotifyPropertyChanged
-    { 
-    private UserControl _currentPage;
-    private RelayCommand _createData;
-
-    public ObservableCollection<UserControl> Pages { get; }
-    public UserControl CurrentPage { get => _currentPage; set { _currentPage = value; OnPropertyChanged(nameof(CurrentPage)); } }
-
-    /// <summary>
-    /// инициализации начальных данных в БД
-    /// </summary>
-    public RelayCommand CreateData
+    public class MainViewModel
     {
-        get => _createData ?? (_createData = new RelayCommand(obj =>
-            { Task.Run(() => (new ContentCreate(new HospitalDbContextFactory()))); }));
-    }
+        private RelayCommand _createData;
 
-    public MainViewModel(UserAccess userAccess)
-    {
-        switch (userAccess)
+        public RelayCommand CreateData
         {
-            case UserAccess.admin:
-                Pages = new ObservableCollection<UserControl>
-                    {
-                        new Views.Registrator(),
-                        new Views.Schedule(),
-
-                        new Views.Ambulatory()
-
-                    };
-                CurrentPage = Pages.ElementAt(0);
-                break;
-            case UserAccess.doctor:
-                break;
-            case UserAccess.registrator:
-                Pages = new ObservableCollection<UserControl>
-                    {
-                        new Views.Registrator(),
-                    };
-                CurrentPage = Pages.ElementAt(0);
-                break;
-            case UserAccess.manager:
-                break;
-            default:
-                break;
+            get => _createData ?? (_createData = new RelayCommand(obj =>
+                { Task.Run(() => (new ContentCreate(new HospitalDbContextFactory()))); }));
         }
     }
-
-    public event PropertyChangedEventHandler PropertyChanged;
-    public void OnPropertyChanged([CallerMemberName] string prop = "")
-    {
-        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
-    }
-
-}
 }
