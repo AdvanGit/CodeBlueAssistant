93221b784f149552a49bbc6876b2f25a980333cc
AdvanGit <advan8912@gmail.com>
2021-01-29 21:18:29 +0500

    InfoPanelScheduleUpdate

---
+++ Hospital.WPF/Services/DateToAgeConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows.Data;
+
+namespace Hospital.WPF.Services
+{
+    class DateToAgeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            DateTime today = DateTime.Today;
+            DateTime birthday = (DateTime)value;
+
+            int years = today.Year - birthday.Year;
+            int month = today.Month - birthday.Month;
+
+            if (today.Day < birthday.Day) month--;
+            if (month < 0) years--;
+
+            return years;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
