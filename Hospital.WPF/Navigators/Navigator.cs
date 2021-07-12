a453cd2ca1075da93f648cae3555452091a5b6b1
AdvanGit <advan8912@gmail.com>
2021-07-13 04:14:01 +0500

    Cleaning Navigator from broken history managment feature

--- Hospital.WPF/Navigators/Navigator.cs
+++ Hospital.WPF/Navigators/Navigator.cs
@@ -1,6 +1,5 @@
 ﻿using Hospital.WPF.Commands;
 using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 
@@ -9,70 +8,29 @@ namespace Hospital.WPF.Navigators
     public class Navigator : INotifyPropertyChanged
     {
         private INavigatorItem _currentBody;
-        private bool isHistory;
-        private List<INavigatorItem> controlsQueue = new List<INavigatorItem>();
-        private int queueIndex;
-        private void AddHistory(INavigatorItem control)
+        public Navigator(ObservableCollection<INavigatorItem> controls)
         {
-            if (isHistory)
-            {
-                if (controlsQueue.Count > 10) controlsQueue.RemoveAt(0);
-                controlsQueue.Add(control);
-                queueIndex = controlsQueue.Count - 1;
-            }
-        }
-
-        public Navigator(ObservableCollection<INavigatorItem> controls, bool isHistory = false)
-        {
-            this.isHistory = isHistory;
             Bodies = controls;
         }
 
-        public INavigatorItem CurrentBody { get => _currentBody; set { _currentBody = value; OnPropertyChanged(nameof(CurrentBody)); AddHistory(value); } }
+        public INavigatorItem CurrentBody { get => _currentBody; set { _currentBody = value; OnPropertyChanged(nameof(CurrentBody)); } }
 
         public ObservableCollection<INavigatorItem> Bodies { get; }
 
         public void SetBody(string typeName)
         {
             foreach (INavigatorItem control in Bodies)
-                if (control.GetType().Name == typeName)
-                {
-                    CurrentBody = control;
-                    AddHistory(control);
-                }
+                if (control.GetType().Name == typeName) CurrentBody = control;
         }
         public void SetBody(Type type)
         {
             foreach (INavigatorItem control in Bodies)
-                if (control.GetType() == type)
-                {
-                    CurrentBody = control;
-                    AddHistory(control);
-                }
-        }
-
-        public void Previous()
-        {
-            if (isHistory && queueIndex > 0)
-            {
-                queueIndex--;
-                CurrentBody = controlsQueue[queueIndex];
-            }
-        }
-        public void Next()
-        {
-            if (isHistory && queueIndex < controlsQueue.Count - 1)
-            {
-                queueIndex++;
-                CurrentBody = controlsQueue[queueIndex];
-            }
+                if (control.GetType() == type) CurrentBody = control;
         }
 
         public Command SetByIndex => new Command(i => CurrentBody = Bodies[int.Parse(i.ToString())], i => int.Parse(i.ToString()) + 1 >= Bodies.Count && int.Parse(i.ToString()) + 1 <= Bodies.Count);
         public Command SetByTypeName => new Command(i => SetBody(i.ToString()), i => i != null);
         public Command SetByType => new Command(i => SetBody((Type)i), i => i != null);
-        public Command PreviousHistory => new Command(i => Previous());
-        public Command NextHistory => new Command(i => Next());
 
         public event PropertyChangedEventHandler PropertyChanged;
         public void OnPropertyChanged(string prop = "") { PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop)); }
